d5c8e42ad9b719112869ce95a57df5e3d12b3bdd
Jeff Hui <contrib@jeffhui.net>
2010-04-28 23:45:02 -0400

    Version 1.4; See changelog file.

--- jquery.blank.js
+++ jquery.blank.js
@@ -4,18 +4,7 @@
  *  o Ashish - ashish@setfive.com
  *  o Sherri Alexander - sherri-alexander.com
  * =============
- * vim: tabstop=4;shiftwidth=4;noexpandtab
- * =============
- * Changelog:
- *
- * --- Version 1.3
- *  o Refactored code
- *  o Changed onsubmit behavior for blank text to be empty string
- *	  (credits: Ashish - ashish@setfive.com)
- *
- * --- Version 1.2
- *  o Created changelog
- *  o Fixed indexof bug for IE (reported by Sherri Alexander)
+ * Changelog: See changelog file
  * =============
  * Examples:
  *    $('#id').blank('default text');
@@ -43,6 +32,15 @@
  *	                  similar kind of effect with the login textboxes.
  *	                  Defaults to false.
  * =============
+ * jquery.blank adds the following events for the elements that have .blank(text, options)
+ * called upon them.
+ *
+ * Events:
+ *  blank:            This is called when the text becomes blank. This behavior is triggered
+ *                    onkeyup if defaultUntilType is true and onblur otherwise.
+ *  fill:             This is called when the text field becomes filled by user input. This
+ *	                  is triggered onkeydown if defaultUntilType is true and onblur otherwise.
+ * =============
  * Copyright (c) 2009 Jeff Hui <contrib@jeffhui.net>
  * 
  * Permission is hereby granted, free of charge, to any person
@@ -66,37 +64,40 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  *
+ * =============
+ * vim: tabstop=4;shiftwidth=4;noexpandtab
  */
 (function($){
 
-// assume: the user knows this only should be used only with text-typing elements
-$.fn.blank = function(text, options){
+// keys that we ignore for keypress (for defaultUntilType)
+var ignoreList = [
+	8,  // backspace
+	9,  // tab
+	16, // shift
+	17, // alt
+	18, // pause/break
+	20, // capslock
+	27, // esc
+	33, // pgup
+	34, // pgdown
+	35, // end
+	36, // home
+	91, // left winkey
+	92, // right winkey
+	144,// numlock
+	145 // scroll lock
+];
+// keys that do a movement in the textbox that we should suppress
+// we can assume we're at the left-most side
+var moveList = [
+	34, 35 // pgdown, end
+];
 
-// when we use $(el).blank() # returns a true/false if the input is actually blank
-if(text === undefined){
-	var d = this.data('blank');
-	return (d !== undefined);
-}
+var fill_triggername = 'fill',
+	blank_triggername = 'blank';
 
-var opt = {
-	defaultCSS: {color: '#999'},
-	normalCSS: function(el, defCSS, defFocusCSS){
-		// here we just try to get default css styles from the element
-		// for all key values of defCSS
-		var css = $.extend({}, defCSS, defFocusCSS);
-		var e = $(this);
-		$.each(defCSS, function(i, v){
-			css[i] = $.curCSS(e.get(0), i);
-		});
-		return css;
-	},
-	defaultFocusCSS: {color: '#bbb'},
-	defaultUntilType: false
-};
-$.extend(opt, options);
-
-// it's outside the each call since less function defs inside the each call
-// makes things faster. It's minor, but I like var defs at the start of bodies.
+// Gets the value of func, calling it if its a function. el nas param
+// are values passed into it.
 function call(func, el, param){
 	if($.isFunction(func))
 		return (param != undefined) ? func.call(el, el, param) : func.call(el, el);
@@ -104,105 +105,192 @@ function call(func, el, param){
 		return func;
 }
 
-return this.each(function(){
-	// css calls are in here, instead of out this call because
-	// option could be a function; which requires the element parameter
-	var defCSS = call(opt.defaultCSS, this),
-		defFocusCSS = call(opt.defaultFocusCSS, this, defCSS),
-		normCSS = call(opt.normalCSS, this, defCSS, defFocusCSS),
-		el = $(this);
-	if(!el.is('input[type=text], textarea'))
-	  return true; // continue
+// moves the text caret for the element to beginning of the textfield.
+// accepts the raw element (not the jQuery object).
+// Optionally accepts an optional position to set the caret to.
+function reset_caret(e, pos){
+	if(pos === undefined)
+		pos = 0;
+	if(e.selectionStart !== undefined){ // W3C compliant browsers
+		e.selectionStart = pos;
+		e.selectionEnd = pos;
+	} else if(e.createTextRange){ // IE-browsers
+		var tr = e.createTextRange();
+		tr.collapse(true);
+		tr.moveStart('character', pos);
+		tr.moveEnd('character', pos);
+		tr.select();
+	}
+}
 
-	// contrib: ashish@setfive.com
-	// if the elements are inside a form, revert to blank text
-	var form = $(this).closest('form');
-	if(form){
-		form.submit(function(){
+
+////// main function //////
+// assume: the user knows this only should be used only with text-typing elements
+$.fn.blank = function(text, options){
+
+	// when we use $(el).blank() # returns a true/false if the input is actually blank
+	if(text === undefined){
+		var d = this.data('blank');
+		return (d !== undefined);
+	}
+
+	// gather options
+	var opt = {
+		defaultCSS: {color: '#999'},
+		normalCSS: function(el, defCSS, defFocusCSS){
+			// here we just try to get default css styles from the element
+			// for all key values of defCSS
+			var css = $.extend({}, defCSS, defFocusCSS);
+			var e = $(this);
+			$.each(defCSS, function(i, v){
+				css[i] = $.curCSS(e.get(0), i);
+			});
+			return css;
+		},
+		defaultFocusCSS: {color: '#bbb'},
+		defaultUntilType: false
+	};
+	$.extend(opt, options);
+
+	// function to work on each element we got
+	return this.each(function(){
+		if(!$(this).is('input[type=text], input[type=password], textarea'))
+		  return true; // continue
+
+		// css calls are in here, instead of out this call because
+		// option could be a function; which requires the element parameter
+		var defCSS = call(opt.defaultCSS, this),
+			defFocusCSS = call(opt.defaultFocusCSS, this, defCSS),
+			normCSS = call(opt.normalCSS, this, defCSS, defFocusCSS),
+			el = $(this),
+			elBlankForm; // used by password fields only
+
+		// Ashish Datta ashish@setfive.com
+		// If the current text is blank text, wipe it since it might be a CTRL+R refresh
+		if(el.val() === text)
+			el.val('');
+
+		// request from: Ashish Datta ashish@setfive.com
+		// Password field support:
+		// We can't change the type=password attribute, so we have to use a new textfield
+		// that is swapped when we need to show default text.
+		if(el.is(':password')){
+			elBlankForm = el.after('<input type="text" class="jqblank_field" />').next();
+			elBlankForm.blank(text, options);
+			el.hide();
+
+			if(opt.defaultUntilType){
+				elBlankForm.bind('fill', function(){
+					elBlankForm.hide();
+					el.val(elBlankForm.val()).show().focus();
+					reset_caret(el, 1);
+				});
+				el.keyup(function(evt){
+					if(el.val() === ''){
+						el.hide();
+						elBlankForm.val('').show().focus().trigger('keyup', [evt]);
+						reset_caret(elBlankForm, 1);
+					}
+				});
+			} else {
+				elBlankForm.focus(function(){
+					elBlankForm.hide();
+					el.show().focus();
+				});
+				el.blur(function(){
+					if($.trim(el.val()) === ''){
+						el.hide();
+						elBlankForm.show().blur();
+					}
+				});
+			}
+		}
+
+		// ashish@setfive.com
+		// if the elements are inside a form, revert to blank text
+		el.closest('form').submit(function(){
 			if(el.blank())
 				el.val('');
 			return true;
+		}).bind('reset', function(){
+			setTimeout(function(){
+				if(el.is(':password')){
+					if(opt.defaultUntilType)
+						el.hide().next().show().blur().keyup();
+					else
+						el.focus().val('').keyup().blur();
+				} else
+					el.keyup().blur();
+			}, 1);
 		});
-	}
-	// end contrib
 
-	if(!opt.defaultUntilType){
+		// don't continue if we're a password field
+		if(elBlankForm)
+			return true; // continue
+		
+		if(!opt.defaultUntilType){
 
-		el.blur(function(){
-			if($.trim(el.val()) === ''){
-				el.val(text).css(defCSS).data('blank', 1);
-			} else {
-				el.removeData('blank');
-			}
-		}).focus(function(){
-			if(el.blank()){
-				el.val('').css(normCSS);
-			}
-		}).blur();
+			el.blur(function(){
+				var isBlank = el.data('blank');
+				if($.trim(el.val()) === ''){
+					el.val(text).css(defCSS).data('blank', 1);
+					if(!isBlank)
+						el.trigger(blank_triggername);
+				} else {
+					el.removeData('blank');
+					if(isBlank)
+						el.trigger(fill_triggername);
+				}
+			}).focus(function(){
+				if(el.blank())
+					el.val('').css(normCSS);
+			}).blur();
 
-	} else { // if !opt.defaultUntilType
+		} else { // opt.defaultUntilType
 
-		el.data('blank', 1);
-		el.blur(function(){
-			if(el.data('blank')){
-				el.css(defCSS).val(text);
-			}
-		}).mouseup(function(){
+			el.data('blank', 1);
+			el.blur(function(){
+				if(el.data('blank')){
+					el.css(defCSS).val(text);
+				}
+			}).mouseup(function(){
 
-			if(!el.blank()) return true;
-			var e = el.get(0);
-			if(e.selectionStart !== undefined){ // W3C compliant browsers
-				e.selectionStart = 0;
-				e.selectionEnd = 0;
-			} else if(e.createTextRange){ // IE-browsers
-				var tr = e.createTextRange();
-				tr.collapse(true);
-				tr.moveStart('character', 0);
-				tr.moveEnd('character', 0);
-				tr.select();
-			}
+				if(!el.blank()) return true;
+				reset_caret(el.get(0));
 
-		}).focus(function(){
+			}).focus(function(){
 
-			if(el.blank()){
-				el.css(defFocusCSS);
-			}
+				if(el.blank()){
+					el.css(defFocusCSS);
+					// disable text selection on tab-focus
+					setTimeout(function(){reset_caret(el.get(0));}, 1);
+				}
 
-		}).keydown(function(evt){
+			}).keydown(function(evt){
 
-			var ignoreList = [
-			8,  // backspace
-			9,  // tab
-			16, // shift
-			17, // alt
-			18, // pause/break
-			20, // capslock
-			27, // esc
-			33, // pgup
-			34, // pgdown
-			35, // end
-			36, // home
-			91, // left winkey
-			92, // right winkey
-			144,// numlock
-			145 // scroll lock
-			]
-			if($.inArray(evt.keyCode, ignoreList) !== -1) return true;
-			var v = el.val();
-			if(el.blank()){
-				el.removeData('blank').css(normCSS).val(v.substring(0, v.length-text.length));
-			}
+				if($.inArray(evt.keyCode, ignoreList) !== -1){
+					if(el.blank() &&  $.inArray(evt.keyCode, moveList))
+						setTimeout(function(){reset_caret(el.get(0))}, 1);
+					return true;
+				}
+				var v = el.val();
+				if(el.blank()){
+					el.val(v.substring(0, v.length-text.length)).removeData('blank')
+					.css(normCSS);
+					el.trigger(fill_triggername);
+				}
 
-		}).keyup(function(){
+			}).keyup(function(){
 
-			var v = el.val();
-			if(!el.blank() && v === ''){
-				el.data('blank', 1).css(defFocusCSS);
-			}
+				var v = el.val();
+				if(!el.blank() && v === ''){
+					el.data('blank', 1).css(defFocusCSS).trigger(blank_triggername);
+				}
 
-		}).blur();
-	} // if-else
-}); // return this.each(function(){})
+			}).blur();
+		} // if-else
+	}); // return this.each(function(){})
 
 }; // $.fn.blank = function(){}
+
 })(jQuery);
